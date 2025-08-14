cc9d3b6aa4fe65c7ea2fd72e9ff1c21b9f0f7bb9
Nick <nickcherny2@gmail.com>
2025-08-14 08:28:03 +0200

    add struct logic

--- word_count/word_count.c
+++ word_count/word_count.c
@@ -1,8 +1,13 @@
 #include <ctype.h>
 #include "stdio.h"
 
+#define MAX_WORDS 1000
+#define MAX_CHARS 1000
 
-
+typedef struct {
+    char word[MAX_CHARS];
+    int count = 0;
+} S_WORDS;
 
 
 
@@ -11,18 +16,17 @@
 int main () {
 
     const char * text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
-    char word_count[1000][1000];
     const char * ptr_text = text;
-    const char * ptr_word = text;
+    S_WORDS words[MAX_WORDS];
 
     int i = 0, j = 0;
     while (*ptr_text) {
         while( *ptr_text && ! isspace(*ptr_text)) {
-         word_count[i][j] = *ptr_text;
+         words[i].word[j] = *ptr_text;
             j++;
             ptr_text++;
         }
-        word_count[i][j] = '\0';
+        words[i].word[j] = '\0';
         j = 0;
         i++;
         ptr_text++;
@@ -30,7 +34,7 @@ int main () {
 
 
     for(int n = 0; n < i; n++) {
-        printf("%s\n", word_count[n]);
+        printf("%s\n", words[n].word);
     }
 
     return 0;
